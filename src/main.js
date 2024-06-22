8efca75eab035ae51afc4f179c757489e67e4471
Petr <kpa80@yandex.ru>
2024-06-22 15:40:38 +0200

    Fixed.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import './assets/main.css'
 
-import cors from 'cors';
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -31,11 +29,4 @@ app.provide('eventStore', useEventStore());
 app.provide('userStore', useUserStore());
 const router = createMyRouter();
 app.use(router);
-app.mount('#app');
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+app.mount('#app')
