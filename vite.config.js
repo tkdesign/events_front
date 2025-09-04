4fa6fb10343ce883c037dfa443fd239f5ef961f1
Petr <kpa80@yandex.ru>
2025-09-04 12:58:40 +0200

    Refactored Vite configuration to load environment variables for allowed hosts.

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        plugins: [
+            vue(),
+        ],
+        resolve:
+        {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+        server: {
+            allowedHosts: [env.VITE_API_BASE_URL.replace(/^https?:\/\//, ''), 'localhost'],
+                host: '0.0.0.0',
+                port: 3000,
+        }
     }
-  },
-  server: {
-    allowedHosts: ['.events.local', 'localhost'],
-    host: '0.0.0.0',
-    port: 3000,
-  }
 })
